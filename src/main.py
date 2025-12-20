c14a0f5036a83f19e909f84722f5bdbcfa7b486d
Shuaib Adams <shuaibadams2000@gmail.com>
2025-12-20 19:31:14 +0200

    Add basic test form for flag game
    Added a simple form to test flag-to-country game logic.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from operator import index
+
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import pandas
 
 app = Flask(__name__)
 Bootstrap(app)
+df = pandas.read_csv("static/data/countries.csv")
+print(df)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/country-from-flag')
-def country_from_flag():
-    return render_template("country_from_flag.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route('/country-from-flag', methods=["GET", "POST"])
+# def country_from_flag():
+#     if request.method == "POST":
+#         country_name_input = request.form.get("country-name-input")
+#         print(country_name_input)
+#     return render_template("country_from_flag.html")
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
